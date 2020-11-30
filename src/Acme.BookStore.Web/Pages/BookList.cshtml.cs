474e5b1b77079c2d9abe3b28c5df5dfc2ebcee47
Tam Minh <minhtam110397@gmail.com>
2020-11-30 20:40:51 +0700

    show books to user

---
+++ src/Acme.BookStore.Web/Pages/BookList.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ABPBlog.Web.Pages
+{
+    public class PostListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- src/Acme.BookStore.Web/Pages/Index.js
+++ src/Acme.BookStore.Web/Pages/Index.js
@@ -1,3 +1,48 @@
 ﻿$(function () {
-    abp.log.debug('Index.js initialized!');
+    var l = abp.localization.getResource('BookStore');
+
+    var dataTable = $('#AuthorsTable').DataTable(
+        abp.libs.datatables.normalizeConfiguration({
+            serverSide: true,
+            paging: true,
+            order: [[1, "asc"]],
+            searching: false,
+            scrollX: true,
+            ajax: abp.libs.datatables.createAjax(acme.bookStore.books.book.getList),
+            columnDefs: [
+
+                {
+                    data: { name: "name", authorName: "authorName", id: "id" },
+                    "render": function (data) {
+                        return '<a  href="/BookList?' + data.id + '&type=name"><span class="list_hshv_name">' + data.name + '</a>'
+                    }
+                }, 
+
+                //{
+                //    title: l('Name'),
+                //    data: "name"
+                //},
+                //{
+                //    title: l('BirthDate'),
+                //    data: "birthDate",
+                //    render: function (data) {
+                //        return luxon
+                //            .DateTime
+                //            .fromISO(data, {
+                //                locale: abp.localization.currentCulture.name
+                //            }).toLocaleString();
+                //    }
+                //},
+                  {
+                    title: l('Name'),
+                    data: "name"
+                },
+                // ADDED the NEW AUTHOR NAME COLUMN
+                {
+                    title: l('Author'),
+                    data: "authorName"
+                }
+            ]
+        })
+    );
 });
