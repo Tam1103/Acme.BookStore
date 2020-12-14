5a80ef642f6357c00c624828a3f6e329ebc2d96c
Tam Minh <minhtam110397@gmail.com>
2020-12-14 09:55:34 +0700

    edit indexmodel in slides

--- src/Acme.BookStore.Web/Pages/Slides/Index.cshtml.cs
+++ src/Acme.BookStore.Web/Pages/Slides/Index.cshtml.cs
@@ -1,10 +1,15 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Acme.BookStore.Slides;
+using System.Collections.Generic;
 
 namespace Acme.BookStore.Web.Pages.Slides
 {
     public class IndexModel
-    { 
+    {
+        public IReadOnlyList<SlideDto> Slides { get; }
+        public IndexModel(IReadOnlyList<SlideDto> Slide)
+        {
+            Slides = Slide;
+        }
         public void OnGet()
         {
         }
